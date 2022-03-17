c6cd34f7779341d2f7f990755d30efbb935feca9
Colin Gross <grosscol@gmail.com>
2022-03-17 14:55:57 -0400

    Migrate to terraform cloud backend.

--- provision/versions.tf
+++ provision/versions.tf
@@ -1,15 +1,22 @@
 terraform {
+  cloud {
+    organization = "statgen"
+    workspaces {
+      name = "bravo-ci-staging"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.20.0"
+      version = ">= 3.20"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "3.0.0"
+      version = ">= 3.0"
     }
   }
 
-  required_version = "~> 1.0"
+  required_version = ">= 1.1.0"
 }
